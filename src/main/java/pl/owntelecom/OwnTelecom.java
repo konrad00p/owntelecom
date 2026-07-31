29cb39d1663d34f97a9ccda633a75f8f5f4fa721
konrad00p <62291216+konrad00p@users.noreply.github.com>
2026-07-31 16:06:01 +0200

    Create OwnTelecom.java

---
+++ src/main/java/pl/owntelecom/OwnTelecom.java
@@ -0,0 +1,97 @@
+package pl.owntelecom;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import pl.owntelecom.commands.*;
+import pl.owntelecom.listeners.ChatListener;
+import pl.owntelecom.managers.*;
+
+public final class OwnTelecom extends JavaPlugin {
+
+    private static OwnTelecom instance;
+    private Economy economy;
+    
+    // Managery
+    private ConfigManager configManager;
+    private OperatorManager operatorManager;
+    private StationManager stationManager;
+    private CallManager callManager;
+    private InternetManager internetManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Inicjalizacja Vault
+        if (!setupEconomy()) {
+            getLogger().severe("Vault nie został znaleziony! Plugin zostanie wyłączony.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        
+        // Zapisz domyślne pliki konfiguracyjne
+        saveDefaultConfig();
+        
+        // Inicjalizacja managerów
+        this.configManager = new ConfigManager(this);
+        this.operatorManager = new OperatorManager(this);
+        this.stationManager = new StationManager(this);
+        this.callManager = new CallManager(this);
+        this.internetManager = new InternetManager(this);
+        
+        // Rejestracja komend
+        registerCommands();
+        
+        // Rejestracja listenerów
+        registerListeners();
+        
+        getLogger().info("OwnTelecom został pomyślnie włączony!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Zapisanie danych przed wyłączeniem
+        if (operatorManager != null) operatorManager.saveAll();
+        if (stationManager != null) stationManager.saveAll();
+        
+        getLogger().info("OwnTelecom został wyłączony.");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
+    }
+
+    private void registerCommands() {
+        new TelefonCommand(this);
+        new CallCommand(this);
+        new SmsCommand(this);
+        new OdbierzCommand(this);
+        new RozlaczCommand(this);
+        new AlarmowyCommand(this);
+        new OperatorCommand(this);
+        new StacjaCommand(this);
+        new InternetCommand(this);
+    }
+
+    private void registerListeners() {
+        getServer().getListenerRegistrar().register(new ChatListener(this));
+    }
+
+    // Gettery
+    public static OwnTelecom getInstance() { return instance; }
+    public Economy getEconomy() { return economy; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public OperatorManager getOperatorManager() { return operatorManager; }
+    public StationManager getStationManager() { return stationManager; }
+    public CallManager getCallManager() { return callManager; }
+    public InternetManager getInternetManager() { return internetManager; }
+}
